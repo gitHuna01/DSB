8403363d19b4c88d7b6ae5ada393400aa09a75fd
gitHuna01 <biman70070@khu.ac.kr>
2026-06-29 05:42:58 +0900

    Add files via upload

--- DSB.py
+++ DSB.py
@@ -16,6 +16,11 @@ class DummyHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Bot is perfectly running 24/7!")
 
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
+
 def run_dummy_server():
     port = int(os.environ.get("PORT", 10000))
     server = HTTPServer(('0.0.0.0', port), DummyHandler)
